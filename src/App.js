c27cdbb973d3dd3fa508287276b0feb66638458c
Yulian <yefymenko@zeo.ua>
2021-07-01 12:04:57 +0300

    add JsTransition to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import JsTransition from './components/JsTransition';
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <h2>Js transition.</h2>
+        <JsTransition/>
+      </div>
+
+
     </div>
   );
 }
--- src/components/JsTransition/JsTransition.js
+++ src/components/JsTransition/JsTransition.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import BoxComponent from "../BoxComponent";
 export default function JsTransition() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [isShow, setIsShow] = useState(false);
+  const toggleIsShow = () => {
+    setIsShow((show) => !show);
+  };
+  const clickHandler = () => {
+    toggleIsShow();
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={clickHandler}>{isShow ? "hide" : "show"}</button>
+      </div>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <BoxComponent />
+        <BoxComponent />
+      </div>
+    </>
+  );
 }
